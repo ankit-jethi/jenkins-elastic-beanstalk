41f9509f403a37a50d30906c5f54e6a8dc563064
ankit-jethi <53268118+ankit-jethi@users.noreply.github.com>
2020-06-14 20:59:31 +0530

    Update the repo name

--- install/setup.sh
+++ install/setup.sh
@@ -7,8 +7,7 @@ sudo pip install --upgrade pip
 sudo pip install virtualenv
 
 # Clone the app from GitHub
-cd /opt/
-git clone https://github.com/ankit-jethi/python-news-app.git
+git clone https://github.com/ankit-jethi/jenkins-elastic-beanstalk.git /opt/python-news-app
 cd /opt/python-news-app/
 
 # Create a Virtual Environment for the app
@@ -19,13 +18,13 @@ source /opt/python-news-app/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
 
-# Deactivate
+# Deactivate the virtual environment
 deactivate
 
 # Create systemd service
 sudo cp /opt/python-news-app/install/python-news-app.service /etc/systemd/system/python-news-app.service
 
-# Create directory for storing gunicorn logs
+# Create a directory for storing gunicorn logs
 sudo mkdir -p /var/log/python-news-app/gunicorn/
 sudo chown -R ec2-user:ec2-user /var/log/python-news-app/
 
@@ -33,4 +32,3 @@ sudo chown -R ec2-user:ec2-user /var/log/python-news-app/
 sudo systemctl daemon-reload
 sudo systemctl start python-news-app
 sudo systemctl enable python-news-app
-
